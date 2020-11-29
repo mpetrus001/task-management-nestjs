de6645471b2bc3ed876edd36d2198b4a9ce81b83
MatthewP <mpetrus001@gmail.com>
2020-11-29 10:58:29 -0600

    fixed bug in Content-Range intercept when receiving 0 tasks

--- src/tasks/interceptor/res-headers.interceptor.ts
+++ src/tasks/interceptor/res-headers.interceptor.ts
@@ -3,21 +3,33 @@ import {
   NestInterceptor,
   ExecutionContext,
   CallHandler,
+  Logger,
 } from '@nestjs/common';
 import { Response } from 'express';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Injectable()
 export class ResponseHeaderInterceptor implements NestInterceptor {
+  private logger = new Logger('ContentRangeIntercept');
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const httpContext = context.switchToHttp();
     const httpRequest = httpContext.getRequest<Request>();
     const httpResponse = httpContext.getResponse<Response>();
     return next.handle().pipe(
       map(({ data, range, count }) => {
-        if (count !== undefined || count !== null) {
-          const resource = httpRequest.url.split('/')[1].split('?')[0];
+        if (Array.isArray(data)) {
+          if (count == undefined || count == null) {
+            this.logger.warn(`Expected count but received null or undefined`);
+            this.logger.warn(`No Content-Range header added`);
+            return data;
+          }
+          if (range == undefined || range == null) {
+            this.logger.warn(`Expected range but received null or undefined`);
+            this.logger.warn(`No Content-Range header added`);
+            return data;
+          }
+          const resource = httpRequest.url.split('/')[2].split('?')[0];
           httpResponse.setHeader(
             'Content-Range',
             `${resource} ${range[0]}-${range[1]}/${count}`,
